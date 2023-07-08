9844533167088beabdde2997d0fb680da514e96a
Vincent <vincent854321@gmail.com>
2023-07-08 18:59:55 -0500

    added element and todo class

--- src/createtodos.js
+++ src/createtodos.js
@@ -7,4 +7,90 @@ class todo{
         this.date = date;
     }
 
+    get priority(){
+        return this.priority;
+    }
+
+    get title(){
+        return this.title;
+    }
+    
+    get description(){
+        return this.description;
+    }
+
+    get date(){
+        return this.date;
+    }
+}
+
+export class Element {
+  constructor(elementType) {
+    this.elementType = elementType;
+    this.attributes = {};
+    this.children = [];
+    this.eventListeners = {};
+  }
+
+  buildElement() {
+    // Create element in DOM
+    const realBoi = document.createElement(this.elementType);
+
+    // Add attributes to element
+    for (const attribute in this.attributes) {
+      if (
+        this.attributes[attribute] === true ||
+        this.attributes[attribute] === false
+      ) {
+        realBoi.toggleAttribute(attribute, this.attributes[attribute]);
+      } else {
+        realBoi.setAttribute(attribute, this.attributes[attribute]);
+      }
+    }
+
+    // Add event listeners:
+    for (const ev in this.eventListeners) {
+      this.eventListeners[ev].forEach((cb) => {
+        realBoi.addEventListener(ev, cb);
+      });
+    }
+
+    // Append children? Append text?
+    if (this.text === undefined) {
+      for (const virtualBoi of this.children) {
+        realBoi.appendChild(virtualBoi.buildElement());
+      }
+    } else {
+      const realText = document.createTextNode(this.text);
+      realBoi.appendChild(realText);
+    }
+    return realBoi;
+  }
+
+  setAttributes(object) {
+    Object.assign(this.attributes, object);
+    return this;
+  }
+
+  addChild(element) {
+    this.text = undefined;
+    this.children.push(element);
+
+    return this;
+  }
+
+  setTextContent(string) {
+    this.children = [];
+    this.text = string;
+    return this;
+  }
+
+  appendEventListener(event, callback) {
+    if (this.eventListeners[event]) {
+      this.eventListeners[event].push(callback);
+    } else {
+      this.eventListeners[event] = [callback];
+    }
+    return this;
+  }
 }
