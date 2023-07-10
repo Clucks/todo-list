bc3192dc869997c66633969af43704521946cf47
Vincent <vincent854321@gmail.com>
2023-07-10 15:53:29 -0500

    created a projects tab and project class

--- src/initialdom.js
+++ src/initialdom.js
@@ -3,6 +3,7 @@ import { Element } from "./classes";
 export const initialdom = () => {
     const content = document.querySelector('#content');
 
+    //creates the header for the inital dom
     const header = new Element('div');
     header
         .setAttributes({id: "header"})
@@ -12,8 +13,26 @@ export const initialdom = () => {
             .setTextContent('When2do')
             .setAttributes({id: "title"}))
 
+
+    //creates the projects tab to create a new list
+    const container = new Element('div');
+    container
+        .setAttributes({id: "container"})
+        .addChild(new Element('span')
+            .setTextContent("Projects"))
+        .addChild(new Element('button')
+            .setTextContent("Add new project")
+                .appendEventListener("click", function(){
+                //Should create a new todo in the project
+
+                })) 
+        .addChild(new Element('div'))
+    
+    //appends and builds all the doms
     const headerdom = header.buildElement(); 
+    const containerdom = container.buildElement();
     content.appendChild(headerdom);
+    content.appendChild(containerdom)
     
 
 
